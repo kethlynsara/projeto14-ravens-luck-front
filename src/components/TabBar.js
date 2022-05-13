66ce83ea262b4fc4ec017935337c1a779cdf71df
Henrique Butzke Kotz <henriquebkotz@gmail.com>
2022-05-13 18:38:57 -0300

    redirect to sign-up page

--- src/components/TabBar.js
+++ src/components/TabBar.js
@@ -1,15 +1,29 @@
+import { useContext } from 'react';
 import styled from 'styled-components';
 import { RiHomeLine, RiShoppingCart2Line, RiSettings4Line, RiBookmarkLine } from 'react-icons/ri';
 import { Link } from 'react-router-dom';
 
+import UserContext from '../contexts/UserContext';
+
 function TabBar({icon}) {
+    const { userInfo } = useContext(UserContext);
+    const redirect = userInfo.token === "";
+
     return (
             <Footer>
                 <Icons>
-                    <StyledLink to="/"><RiHomeLine style={icon === 'home' ? {color: "#000"} : {color: "#9C9EA8"}} /></StyledLink>
-                    <StyledLink to="wishlist"><RiBookmarkLine style={icon === 'bookmark' ? {color: "#000"} : {color: "#9C9EA8"}} /></StyledLink>
-                    <StyledLink to="/cart"><RiShoppingCart2Line /></StyledLink>
-                    <StyledLink to="/settings"><RiSettings4Line /></StyledLink>
+                    <StyledLink to='/'>
+                        <RiHomeLine style={icon === 'home' ? {color: "#000"} : {color: "#9C9EA8"}} />
+                    </StyledLink>
+                    <StyledLink to={userInfo.token === '' ? '/sign-in' : '/wishlist'}>
+                        <RiBookmarkLine style={icon === 'bookmark' ? {color: "#000"} : {color: "#9C9EA8"}} />
+                    </StyledLink>
+                    <StyledLink to={userInfo.token === '' ? '/sign-in' : '/cart'}>
+                        <RiShoppingCart2Line />
+                    </StyledLink>
+                    <StyledLink to={userInfo.token === '' ? '/sign-in' : '/settings'}>
+                        <RiSettings4Line />
+                    </StyledLink>
                 </Icons>
             </Footer>
     )
